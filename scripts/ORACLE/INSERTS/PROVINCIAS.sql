803b0e35f7c1f65adab2fa733f38f8ef29cf4b27
Manudo Killer <una.esteban@gmail.com>
2021-03-06 18:32:07 -0600

    init commit

---
+++ scripts/ORACLE/INSERTS/PROVINCIAS.sql
@@ -0,0 +1,9 @@
+insert into proyecto1.provincias (codigo, nombre) values (1, 'SAN JOSE');
+insert into proyecto1.provincias (codigo, nombre) values (2, 'ALAJUELA');
+insert into proyecto1.provincias (codigo, nombre) values (3, 'CARTAGO');
+insert into proyecto1.provincias (codigo, nombre) values (4, 'HEREDIA');
+insert into proyecto1.provincias (codigo, nombre) values (5, 'GUANACASTE');
+insert into proyecto1.provincias (codigo, nombre) values (6, 'PUNTARENAS');
+insert into proyecto1.provincias (codigo, nombre) values (7, 'LIMON');
+
+select * from proyecto1.provincias
